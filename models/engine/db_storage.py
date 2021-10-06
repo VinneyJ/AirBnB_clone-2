e991da13f1b22e15d16d7f72fd7864b8a556a12a
root <vincentjayden49@gmail.com>
2021-10-06 19:44:01 +0000

    New storage DB

---
+++ models/engine/db_storage.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""DBStorage module"""
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+    
+    def __init__(self):
+        """Constructor"""
+        
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")), 
+                                      pool_pre_ping=True)
+        # drop all environment variables if HBNB_ENV is equal to test
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def  all(self, cls=None):
+        """queries on current database session depending on
+        class name arguement
+        """
+        if cls is None:
+            objects = self.__session.query(State).all()
+            objects.extend(self.__session.query(City).all())
+            objects.extend(self.__session.query(User).all())
+            objects.extend(self.__session.query(Place).all())
+            objects.extend(self.__session.query(Review).all())
+            objects.extend(self.__session.query(Amenity).all())
+
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
